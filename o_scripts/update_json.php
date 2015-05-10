759200192fbcab6218ba79db180e46a853129bfd
Alex <alboghardau@gmail.com>
2015-05-10 17:07:31 +0300

    sync

--- o_scripts/update_json.php
+++ o_scripts/update_json.php
@@ -1,16 +1,18 @@
 <?php
 //GENERATE JSON RESPONSE FOR UPDATE REQUEST
-$receivedTimeStamp = $_POST['time'];
-//$receivedTimeStamp = 20150400000000;
+//$receivedTimeStamp = $_POST['time'];
+
+$receivedTimeStamp = 20150400000000;
 
 $data = new PDO("sqlite:../phpliteadmin/answerit.db");
 $sql = $data->prepare("SELECT * FROM quest WHERE time_stamp > ".$receivedTimeStamp);
 $sql->execute();
 $sql->setFetchMode(PDO::FETCH_OBJ);
 
-$json = json_encode($sql->fetchAll());
+$json = json_encode(array('questions' => $sql->fetchAll()));
 echo $json;
 
  
 ?>
+
 
