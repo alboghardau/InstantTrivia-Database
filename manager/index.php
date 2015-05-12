ce806c4f98faefabe657b54ed0194039a645d0f4
Alex <alboghardau@gmail.com>
2015-05-12 22:03:25 +0300

    sync

---
+++ manager/index.php
@@ -0,0 +1,34 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    </head>
+<html>
+    <body>
+ 
+
+<nav>   
+  <ul id="slide-out" class="side-nav fixed">
+    <li><a href="#!">First Sidebar Link</a></li>
+    <li><a href="#!">Second Sidebar Link</a></li>
+  </ul>
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+</nav>    
+
+
+        <div class="row">
+            
+        </div>
+
+        <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script> 
+        
+        <script>
+     ( document ).ready(function(){$(".button-collapse").sideNav();})
+        </script>
+    </body>
+</html>
